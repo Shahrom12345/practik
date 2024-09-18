49a3e7a049d87be080eec8fb60241c6c7defc4d6
Shahrom <167228749+Shahrom12345@users.noreply.github.com>
2024-09-18 17:11:34 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+//  /// 1///
+
+// // function Sum(input) {
+// //     let result = '';
+// //     for (let char of input) {
+// //         result += char.repeat(2);
+// //     }
+// //     return result;
+// // }
+// // const str1 = "Привет"
+// // const str2 = Sum(str1)
+// // console.log(str2)
+
+
+// 2
+//function raqam(numbers) {
+    //     if (numbers.includes(7)) {
+    //         return "Бум!";
+    //     } else {
+    //         return "Не";
+    //     }
+    // }
+ //}
+    // console.log(raqam([1, 2, 3, 4, 5]));
+
+// 3 
+
+// function Sum(num) {
+//     return num.replace(g, '');
+// }
+// let int3 = "Привет 123 мир 456!";
+// let int4 = Sum(num);
+// console.log(int4)
+
+
+// // 4 
+
+// function Sum(num) {
+//     return num.split('').reverse().join('');
+// }
+
+// let sum2 = "Привет мир!";
+// let int = Sum(sum2);
+// console.log(int)
+
+
+//     // 5 
+
+// function Sum(word) {
+//     let result = "";
+//     for (let char of word) {
+//         if (!result.includes(char)) {
+//             result += char;
+//         }
+//     }
+//     return result;
+// }
+// let word = "программирование";
+// let int1 = Sum(word);
+// console.log(int1)
